0d306fe02fa2eb147853ddf1d0bdfe2c749f6ca9
Chiragdbb <schirag414@gmail.com>
2024-07-29 02:26:28 +0530

    Add: routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import HomePage from "./routes/homePage/HomePage"
+import DashboardPage from './routes/dashboardPage/DashboardPage'
+import ChatPage from './routes/chatPage/ChatPage'
+import RootLayout from './Layouts/rootLayout/RootLayout'
+import DashboardLayout from './Layouts/dashboardLayout/DashboardLayout'
+import SignInPage from './routes/signInPage/SignInPage'
+import SignUpPage from './routes/signUpPage/SignUpPage'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route path='' element={<HomePage />} />
+      <Route path='dashboard' element={<DashboardLayout />} >
+        <Route path='' element={<DashboardPage />} />
+        <Route path='chat/:id' element={<ChatPage />} />
+      </Route>
+      <Route path='sign-in/*' element={<SignInPage />} />
+      <Route path='sign-up/*' element={<SignUpPage />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
